0c92e8de27f48000f68abca9426f9871d5daec9a
evannn4k <mevansusanto@gmail.com>
2026-07-18 13:26:13 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,107 @@
+<?php
+
+// spasi html
+// &nbsp;
+
+function factorial($number)
+{
+    if ($number < 0) {
+        echo "angka tidak boleh dibawah 0";
+        return;
+    }
+
+    $result = 1;
+    $realNumber = $number;
+
+    while ($number > 0) {
+        $result = $result * $number;
+        // echo $number;
+        $number--;
+    }
+
+    // return $result;
+
+    echo "<br>";
+    echo $realNumber . "! adalah " . $result;
+    echo "<br>";
+}
+
+
+// function diamond($number)
+// {
+//     if($number < 0) {
+//         echo "angka tidak boleh dibawah 0";
+//         return;
+//     }
+
+//     // bagian atas
+//     for ($i = 0; $i < $number; $i++) {
+
+//         for ($y = $number - 1 ; $y > $i; $y--) {
+//             echo "&nbsp;&nbsp;";
+//         }
+
+//         for ($x = 0; $x < $i + 1; $x++) {
+//             echo "*";
+//         }
+
+//         for ($z = 0; $z < $i; $z++) {
+//             echo "*";
+//         }
+
+//         echo "<br>";
+//     }
+
+//     // bagian bawah
+//     for ($i = 0; $i < $number; $i++) {
+
+//         for ($y = 0; $y < $i + 1; $y++) {
+//             echo "&nbsp;&nbsp;";
+//         }
+
+//         for ($x = $number - 1; $x > $i; $x--) {
+//             echo "*";
+//         }
+
+//         for ($z = $number - 2; $z > $i; $z--) {
+//             echo "*";
+//         }
+
+//         echo "<br>";
+//     }
+// }
+
+
+function diamond($number)
+{
+
+    if ($number < 0) {
+        echo "angka tidak boleh dibawah 0";
+        return;
+    }
+
+    for ($i = 1; $i <= $number; $i++) {
+        for ($skip = $number; $skip > $i; $skip--) {
+            echo "&nbsp;&nbsp;";
+        }
+        for ($bintang = 1; $bintang < $i * 2; $bintang++) {
+            echo "*";
+        }
+        echo "<br>";
+    }
+
+    for ($i = 1; $i <= $number; $i++) {
+        for ($skip = 0; $skip < $i; $skip++) {
+            echo "&nbsp;&nbsp;";
+        }
+        for ($bintang = $number * 2 - 1; $bintang > $i * 2; $bintang--) {
+            echo "*";
+        }
+        echo "<br>";
+    }
+}
+
+
+diamond(5);
+factorial(5);
+// echo factorial(5);
---
+++ penjelasan.md
@@ -0,0 +1,32 @@
+Faktorial
+
+cara kerja function penghitung faktorial
+
+1. Menerima parameter number
+2. Cek apakah negative atau bukan
+3. Loop number jika number lebih dari 0 lalu di decrement
+4. Number yang di loop selalu dikalikan dengan result sampai numbernya = 1 
+
+
+
+
+
+
+Diamond Star
+
+sebenarnya ada 2 versi mas, yang versi pertama itu masih misah antara perulangan kanan dan kirinya, nah yang versi kedua sudah saya refactor menjadi di persingkat
+
+cara kerja function pembuat diamond star
+
+1. Menerima parameter number
+2. Cek apakah negative atau bukan
+3. Perulangan dibagi menjadi dua, yaitu perulangan atas dan perulangan bawah
+
+Perulangan atas
+
+1. Membuat skip column dengan menghitung number yang di decrement
+2. Membuat bintang dengan mengalikan mengalikan number dengan angka 2 dan di increment
+
+Perulangan bawah
+
+1. Sama seperti perulangan atas namun ada logika yang saya balik disini
